699a95a16f269bda25a03145b2595e17897fed99
jochemin <jochemin@gmail.com>
2022-12-01 19:02:09 +0100

    Update confirmed.py

--- confirmed.py
+++ confirmed.py
@@ -4,11 +4,10 @@ from urllib.request import urlopen
 
 conn = connect('./mempool_confirm.db')
 curs = conn.cursor()
-url_start = "https://blockchain.info/tx/"
-url_end = "?format=json"
+url_start = "https://blockstream.info/api/tx/"
 for row in curs.execute('SELECT TXID from Accepted'):
     print(row[0])
-    response = urlopen(url_start + row[0]+url_end)
+    response = urlopen(url_start + row[0])
     data_json = json.loads(response.read())
     print(row[0], ' ',data_json["block_height"])
     mined = str(data_json["block_height"])
