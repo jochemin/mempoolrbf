0be415ba8953c4d20dd189aa67cc3351ab8995b4
jochemin <jochemin@gmail.com>
2022-12-02 09:55:21 +0100

    confirmed.py

--- confirmed.py
+++ confirmed.py
@@ -1,19 +1,32 @@
 from sqlite3 import connect
-import json
-from urllib.request import urlopen
+import subprocess
 
-conn = connect('./mempool_confirm.db')
-curs = conn.cursor()
-url_start = "https://blockstream.info/api/tx/"
-for row in curs.execute('SELECT TXID from Accepted'):
+conn = connect('./mempool_fullrbf.db')
+# necesitamos 2 cursores a la bbdd para no salir del for
+curs1 = conn.cursor()
+curs2 = conn.cursor()
+
+for row in curs1.execute('SELECT TXID from Accepted'):
     print(row[0])
-    response = urlopen(url_start + row[0])
-    data_json = json.loads(response.read())
-    print(row[0], ' ',data_json["block_height"])
-    mined = str(data_json["block_height"])
-    print(mined)
-    curs.execute("UPDATE Accepted SET Mined=? WHERE txid=?",(mined, row[0]))
-    #conn.commit()
-
-
+    command = 'bitcoin-cli getrawtransaction {0} 1 | jq .confirmations'.format (row[0])
+    try:
+        # Comprobamos la salida del comando
+        confirmations = subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True)
+        # Pasamos la variable de byte a string
+        confirmations_string = str(confirmations, 'UTF-8')
+        # Quitamos los espacios
+        confirmations_string = confirmations_string.strip()
+        # Si no hay ningún error en la salida del comando
+        if not "error" in confirmations_string:
+            # Cambiamos el tipo a int para tratarlo como un número
+            print(confirmations_string)
+            int_confirmations = int(confirmations_string)
+            if int_confirmations > 0:
+                # Si encontramos un valor menor lo asignamos a la variable sequence
+                confirmed = 1
+                curs2.execute("UPDATE Accepted SET Mined=? WHERE txid=?",(confirmed, row[0]))
+                conn.commit()
+    except subprocess.CalledProcessError:
+        # Lo mismo me da que me da lo mismo
+        pass
 conn.close()
